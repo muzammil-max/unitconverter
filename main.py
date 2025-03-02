58e8d7f4b03a3af52357d9a10b845caa0eb23868
skiermage <199477924+skiermage@users.noreply.github.com>
2025-03-02 20:01:39 +0500

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import time 
-from streamlit_extras.switch_page_button import switch_page
-from streamlit_extras.metric_cards import style_metric_cards
+from streamlit_extras.metric_cards import style_metric_cards # type: ignore
 
 
 #setting the page
